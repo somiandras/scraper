6a9f93d27c4425d50fcf17515045d0379c7fb844
doktor_justice <somiandras@gmail.com>
2018-04-01 20:43:38 +0200

    Original version

---
+++ cleaner.py
@@ -0,0 +1,197 @@
+#!usr/bin/env python
+#-*- coding: utf-8 -*-
+
+from pymongo import MongoClient
+import pandas as pd
+import numpy as np
+import re
+import json
+
+client = MongoClient()
+db = client.cars
+
+# Dictionary map between HUN and ENG strings
+dictionary = json.load(open('dictionary.json', 'r', encoding='utf-8'))
+
+# Column name translation map
+name_map = {
+    'Ajtók száma': 'Doors',
+    'C-Max': 'C-Max_variant',
+    'Csomagtartó (liter)': 'Trunk capacity liters',
+    'Hajtás': 'Drive',
+    'Hengerűrtartalom (cm³)': 'Capacity',
+    'Kilométeróra állása (km)': 'Mileage km',
+    'Kivitel': 'Form',
+    'Klíma fajtája': 'A/C type',
+    'Műszaki vizsga érvényes': 'Documents valid',
+    'Okmányok jellege': 'Documents type',
+    'Saját tömeg (kg)': 'Own weight kg',
+    'Sebességváltó fajtája': 'Transmission',
+    'Szín': 'Color',
+    'Teljesítmény (LE)': 'Horsepower',
+    'Állapot': 'Condition',
+    'Ár (EUR)': 'Price EUR',
+    'Össztömeg (kg)': 'Total weight kg',
+    'Üzemanyag': 'Fuel',
+    'Évjárat év': 'Manufactured year',
+    'Évjárat hónap': 'Manufactured month'
+}
+
+# Columns to drop from raw data
+unnecessary_columns = [
+    'Akciós ár',
+    'Alaptípus ára',
+    'Alvázszám',
+    'Bérelési lehetőség',
+    'Egyéb költségek',
+    'Extrákkal növelt ár',
+    'Finanszírozás',
+    'Finanszírozás típusa CASCO nélkül',
+    'Finanszírozás típusa CASCO-val',
+    'Futamidő',
+    'Futamidő CASCO nélkül',
+    'Futásidő',
+    'Garancia',
+    'Havi részlet',
+    'Hátsó nyári gumi méret',
+    'Hátsó téli gumi méret',
+    'Kezdőrészlet',
+    'Kezdőrészlet CASCO nélkül',
+    'Kilométeróra állása (Nincsmegadva)',
+    'Kárpit színe (1)',
+    'Kárpit színe (2)',
+    'Szavatossági garancia',
+    'Tető',
+    'Téli gumi méret',
+    '_id',
+    'url',
+    'Átrozsdásodási garancia',
+    'Vételár ()',
+    'Vételár (Ft)',
+    'Vételár (FtHitelkalkulátoritt›)',
+    'Vételár (Árnélkül)',
+    'Teljes vételár',
+    'Nyári gumi méret',
+    'Title',
+    'Id',
+    'Henger-elrendezés',
+    'Havi részlet CASCO nélkül',
+    'Vételár ( CHF)'
+    ]
+
+
+def get_month(string):
+    '''Extract month from manufacture date'''
+    match = re.match('[0-9]{4}\/?([0-9]{2})?', string)
+    if match and match.group(1):
+        return match.group(1)
+    else:
+        return None
+
+
+needs_translation = []
+
+
+def translate(string):
+    '''Get the English version from dictionary dict if it exists'''
+    if string in dictionary:
+        return dictionary[string]
+    else:
+        needs_translation.append(string)
+        return string
+
+
+def get_gears(string):
+    '''Extract number of gears from transmission string if possible'''
+    try:
+        match = re.match('^(Automata|Manuális|Szekvenciális)\s\(([4-7])\sfokozatú[^\)]*\)', string)
+        if match:
+            return int(match.group(2))
+        else:
+            return np.nan
+    except Exception:
+        return np.nan
+
+
+def is_metallic(string):
+    '''Decide if the given color is metallic or basic'''
+    try:
+        words = string.split(' ')
+        return '(metál)' in words
+    except Exception:
+        return np.nan
+
+
+def get_color(string):
+    '''Extract color from string and return translated'''
+    try:
+        words = string.split(' ')
+        return translate(words[0])
+    except Exception:
+        return np.nan
+
+
+def get_shade(color):
+    try:
+        words = color.split(' ')
+        if len(words) > 1:
+            return words[0]
+        else:
+            return 'Regular'
+    except Exception:
+        return np.nan
+
+
+def remove_shade(color):
+    try:
+        words = color.split(' ')
+        if len(words) > 1:
+            name = words[1]
+            return name[:1].upper() + name[1:]
+        else:
+            return color
+    except Exception:
+        return np.nan
+
+
+if __name__ == '__main__':
+    cars = db.cars.find({'Model': 'octavia'})
+    df = pd.DataFrame(list(cars))
+
+    # Drop unneeded rows and columns
+    for col in unnecessary_columns:
+        try:
+            df.drop([col], axis=1, inplace=True)
+        except Exception:
+            pass
+    df.dropna(subset=['Ár (EUR)'], inplace=True)
+
+    # Transform price and manufacturing columns
+    df['Ár (EUR)'] = df['Ár (EUR)'].astype('int')
+    df['Évjárat év'] = df['Évjárat'].apply(lambda x: x[:4])
+    df['Évjárat hónap'] = df['Évjárat'].apply(get_month)
+
+    # Translate remaining columns
+    df.rename(columns=name_map, inplace=True)
+
+    # Translate categorical values
+    df['Drive'] = df['Drive'].apply(translate)
+    df['Form'] = df['Form'].apply(translate)
+    df['A/C type'] = df['A/C type'].apply(translate)
+    df['Transmission type'] = df['Transmission'].apply(translate)
+    df['Gears'] = df['Transmission'].apply(get_gears)
+    df['Documents'] = df['Documents type'].apply(translate)
+    df['Condition'] = df['Condition'].apply(translate)
+    df['Fuel'] = df['Fuel'].apply(translate)
+    df['Paint'] = df['Color'].apply(get_color)
+    df['Shade'] = df['Paint'].apply(get_shade)
+    df['Paint'] = df['Paint'].apply(remove_shade)
+    df['Metallic'] = df['Color'].apply(is_metallic)
+
+    # Drop unnecessary columns
+    df.drop(['Transmission', 'Documents type', 'Évjárat', 'Color', 'Szállítható szem'], axis=1, inplace=True)
+
+    # Save to file
+    df.to_csv('../octavia_20171105.csv', encoding='utf-8', index=False)
+    print('Needs translation:')
+    print(set(needs_translation))
---
+++ scraper.py
@@ -0,0 +1,137 @@
+#!usr/bin/env python
+#-*- coding: utf-8 -*-
+
+import requests
+from bs4 import BeautifulSoup
+import logging
+from datetime import date
+from calendar import monthrange
+import re
+from pymongo import MongoClient
+
+client = MongoClient()
+db = client.cars
+
+
+logging.basicConfig(filename='scrape.log', filemode='w', level='DEBUG')
+
+BASE_URL = 'https://www.hasznaltauto.hu/auto/'
+HEADERS = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36'}
+MODELS = [
+    ('skoda', 'octavia')
+]
+
+
+def get_ad_links(brand, model):
+    '''
+    Download initial page of results list, extract the number of result pages,
+    loop through them and save all the individual ad links to a txt file
+    '''
+    
+    ad_links = []
+    # Initial page to get page numbers
+    model_url = '{0}/{1}/{2}'.format(BASE_URL, brand, model)
+    r = requests.get(model_url, headers=HEADERS)
+    logging.debug('URL: {}, status: {}'.format(model_url, r.status_code))
+    if r.status_code == requests.codes.ok:
+        soup = BeautifulSoup(r.text, 'lxml')
+        last_page_number = int(soup.find(title='Utolsó oldal').text)
+
+        # Loop through results pages and get ad links
+        for page in range(1, last_page_number + 1):
+            url = model_url + '/page{}'.format(page)
+            ad_page = requests.get(url, headers=HEADERS)
+            if ad_page.status_code == requests.codes.ok:
+                soup = BeautifulSoup(ad_page.text, 'lxml')
+                result_items = soup.find_all('div', class_='talalati_lista_head')
+                for result_item in result_items:
+                    link = result_item.find('a')
+                    ad_links.append(link.get('href'))
+            else:
+                logging.error('Cannot get {}'.format(url))
+
+        # Save ad links
+        filename = '{}_links.txt'.format(model)
+        with open(filename, 'w') as f:
+            for link in ad_links:
+                f.write(link + '\n')
+    else:
+        logging.error('Cannot get {}'.format(url))
+
+
+def get_car_details(url):
+    '''Extract car details from HTML and return data in dict'''
+    data = {}
+    data['Id'] = url.strip()[-8:]
+    data['url'] = url.strip()
+    r = requests.get(url, headers=HEADERS)
+    soup = BeautifulSoup(r.text, 'lxml')
+    data['Title'] = soup.find('span', property='p:name').text
+
+    data_table = soup.find('table', class_='hirdetesadatok')
+    cells = data_table.find_all('td')
+
+    for cell_index in range(0, len(cells), 2):
+        key = cells[cell_index].text.strip()
+        value = cells[cell_index + 1].text.strip()
+        cleaned = clean_key_value(key, value)
+        data[cleaned[0]] = cleaned[1]
+    return data
+
+
+def clean_key_value(key, value):
+    '''Clean key-value pair'''
+    numeric_fields = ['Csomagtartó', 'Hengerűrtartalom', 'Kilométeróra állása', 'Saját tömeg', 'Szállítható szem. száma', 'Össztömeg', 'Vételár', 'Ár (EUR)']
+    new_key = key[:-1]
+    new_value = value
+
+    if new_key in numeric_fields:
+        value = re.sub('\xa0Ft', 'Ft', value)
+        value = re.sub('€\s', '', value)
+        value = value.replace(' ', '').replace('.', '')
+        match = re.match('([0-9]*)(.*)?', value)
+        if len(match.group(1)) > 0:
+            new_value = int(match.group(1))
+        else:
+            new_value = ''
+        if new_key != 'Ár (EUR)':
+            new_key = new_key + ' ({})'.format(match.group(2))
+
+    if new_key == 'Ajtók száma':
+        new_value = int(value)
+
+    if new_key == 'Teljesítmény':
+        match = re.match('([0-9]{2,3})\skW,\s([0-9]{2,3})\sLE', value)
+        new_value = int(match.group(2))
+        new_key = new_key + ' (LE)'
+
+    return (new_key, new_value)
+
+
+def scrape(brand, model):
+    '''Loop through ad links and save data to MongoDB collection'''
+    filename = '{}_links.txt'.format(model)
+    with open(filename, 'r') as links:
+        for link in links:
+            check = db.cars.find_one({'url': link.strip()})
+            if check is None:
+                logging.info('New link: {}'.format(link))
+                try:
+                    details = get_car_details(link)
+                    details['Brand'] = brand
+                    details['Model'] = model
+                except Exception as e:
+                    logging.error(link)
+                    logging.error(e)
+                else:
+                    db.cars.update_one({'url': link.strip()}, {'$setOnInsert': details}, upsert=True)
+
+
+if __name__ == '__main__':
+    for brand, model in MODELS:
+        logging.info('Starting {} {}'.format(brand, model))
+        get_ad_links(brand, model)
+
+    for brand, model in MODELS:
+        logging.info('Scraping {} {}'.format(brand, model))
+        scrape(brand, model)
